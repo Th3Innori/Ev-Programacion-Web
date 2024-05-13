585318db2b8a49f6c995923d7480c70363d7a156
Amaru <134552877+Th3Innori@users.noreply.github.com>
2024-05-13 00:23:33 -0400

    Add files via upload

--- Evaluacion/2.2Validacion.js
+++ Evaluacion/2.2Validacion.js
@@ -22,11 +22,28 @@ $(function(){
             alert("El formato de la Contraseña no es válido su contraseña debe tener almenos una mayuscula y numeros");
             $('.txtPassword').focus();
         }
+        else if($.trim($('.txtPassword').val())=="")
+            {
+                alert("Falta especificar la Contraseña");
+                $('.txtPassword').focus();
+            }
+            // permitir que la clave contenga letras y numeros
+            else if(!(/^(?=.*\d)[\d\s()-]{9}$/.test($.trim($('.txtTelefono').val()))))
+            {
+                alert("El formato del Telefono no es valido, longitud de 9 digitos");
+                $('.txtPassword').focus();
+            }
         else
         {
-            alert('Los datos enviados son: \n' + $('.txtEmail').val() + ' '
-                                                + $('.txtPassword').val()
-            )
+            var mensaje = 'Los datos enviados son:\n' +
+                          'Nombre: ' + $('#validationDefault01').val() + '\n' +
+                          'Apellido: ' + $('#validationDefault02').val() + '\n' +
+                          'Correo: ' + $('#validationDefaultUsername').val() + '\n' +
+                          'Teléfono: ' + $('#validationDefault05').val() + '\n' +
+                          'Fecha de Nacimiento: ' + $('#validationDefault06').val() + '\n' +
+                          'Región: ' + $('#validationDefault04 option:selected').text() + '\n' +
+                          'Nivel educacional: ' + $('#Educacion option:selected').text();
+            alert(mensaje);
         }
     })
 })
